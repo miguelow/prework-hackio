1111e822cec7a513bf2ad5e48623b29f18edc1ab
miguel <miguel.owd@gmail.com>
2024-08-30 17:46:17 +0200

    terminado ejercicios funciones

---
+++ SQL/03-funciones-agregacion.sql
@@ -0,0 +1,60 @@
+
+/* MIN y MAX */
+
+-- Ejercicio 1: Encontrar la duración mínima de las pistas en milisegundos.
+select * from 
+
+-- Ejercicio 2: Encontrar la duración máxima de las pistas en milisegundos.
+
+
+-- Ejercicio 3: Encontrar el precio mínimo de las pistas.
+   
+
+-- Ejercicio 4: Encontrar el precio máximo de las pistas.
+   
+
+-- Ejercicio 5: Encontrar la fecha mínima de la factura.
+   
+
+-- Ejercicio 6: Encontrar la fecha máxima de la factura.
+   
+
+/* COUNT y SUM */
+
+-- Ejercicio 7: Contar el número total de pistas.
+   
+
+-- Ejercicio 8: Contar el número de clientes en Brasil.
+   
+
+-- Ejercicio 9: Sumar la duración total de todas las pistas en milisegundos.
+   
+
+-- Ejercicio 10: Contar el número de empleados con el título 'Sales Support Agent'.
+    
+
+-- Ejercicio 11: Sumar el total de las cantidades en las facturas.
+    
+
+/* AVG, STDDEV, VARIANCE */
+
+-- Ejercicio 12: Calcular la duración media de las pistas en milisegundos.
+    
+
+-- Ejercicio 13: Calcular el precio medio de las pistas.
+    
+
+-- Ejercicio 14: Calcular la desviación estándar de la duración de las pistas.
+    
+
+-- Ejercicio 15: Calcular la varianza de la duración de las pistas.
+
+
+-- Ejercicio 16: Calcular la desviación estándar del precio de las pistas.
+
+
+/* CONCAT */
+
+-- Ejercicio 17: Concatenar el nombre y el apellido de los clientes.
+
+
