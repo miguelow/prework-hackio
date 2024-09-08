5c0c2a70abcb84a22cf86d80d49cc27f031c38f4
miguel <miguel.owd@gmail.com>
2024-09-08 13:40:18 +0200

    ejercicio consultas basicas terminado

---
+++ SQL/04-consultas-ordenacion-agrupacion.sql
@@ -0,0 +1,44 @@
+
+-- Ejercicio 1:  Selecciona el nombre de todos los álbumes y ordénalos alfabéticamente.
+
+
+-- Ejercicio 2:  Encuentra el álbum con el mayor número de canciones.
+
+
+-- Ejercicio 3:  Calcula la duración promedio, desviación estándar y varianza por compositor.
+
+
+-- Ejercicio 4: Selecciona el título y el precio de las pistas, y limita los resultados a 10.
+
+   
+
+-- Ejercicio 5: Lista los nombres de los empleados y ordénalos por el ID del empleado en orden descendente.
+
+
+-- Ejercicio 6:  Muestra el número de géneros distintos disponibles en la base de datos.
+
+-- Ejercicio 7: Selecciona el nombre del cliente y el país, limitando los resultados a 5, y ordena por el nombre del cliente.
+
+    
+-- Ejercicio 8: Muestra la cantidad total de clientes agrupados por país.
+
+
+-- Ejercicio 9: Obtén el total de importes de facturas para cada país y muestra solo aquellos con un total superior a 10,000.
+
+
+-- Ejercicio 10: Encuentra los países donde el número total de clientes es mayor a 5. Muestra el país y el número de clientes.
+
+
+-- Ejercicio 11: Obtén la cantidad de pistas en cada tipo de medio (MediaType) que tienen una duración mayor a 250,000 milisegundos.
+   
+
+-- Ejercicio 12: Lista el país y el número total de clientes que han sido asistidos por un representante de ventas específico, cuyo ID sea menor a 5.
+   
+
+-- Ejercicio 13: Muestra los códigos postales (BillingPostalCode) de los clientes que han hecho más de 5 compras.
+  
+
+-- Ejercicio 14: Encuentra la cantidad total de pistas en cada tipo de medio (MediaTypeId) donde el precio de la pista (UnitPrice) es mayor a 0.99 y la duración de la pista (Milliseconds) es mayor a 200,000. Muestra el tipo de medio y el número total de pistas.
+
+
+-- Ejercicio 15: Lista el país y el estado de los clientes que tienen el soporte asignado a un representante con ID mayor a 3 y cuyo nombre de empresa (Company) no es nulo. Muestra el país, el estado y el número de clientes.
