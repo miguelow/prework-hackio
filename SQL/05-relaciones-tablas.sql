a9f1eca0fbc3e9e41fbdd1f58dacf58f0a341e00
miguel <miguel.owd@gmail.com>
2024-09-16 20:20:33 +0200

    first exercise in joining exercises done

--- SQL/05-relaciones-tablas.sql
+++ SQL/05-relaciones-tablas.sql
@@ -1,6 +1,10 @@
 
 -- Ejercicio  1: Encuentra el nombre de las pistas y los títulos de los álbumes a los que pertenecen.
-
+SELECT t."Name" AS "Track_Name", a."Title" AS "Album_Title"
+FROM "Track" t
+JOIN "Album" a ON t."AlbumId" = a."AlbumId"
+ORDER BY "Album_Title", "Track_Name";
+ 
 
 -- Ejercicio 2: Lista los nombres de los artistas y los títulos de sus álbumes. Ordena los resultados por artista.
 
